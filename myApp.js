8d8170ae5c0ac8d13a46b9b96bf7f3cc8e2a30c7
Bhaba <8bhaba8@gmail.com>
2024-01-18 18:47:24 +0530

    Adding modified files for express project

--- myApp.js
+++ myApp.js
@@ -1,7 +1,16 @@
 let express = require('express');
 let app = express();
 
+app.get('/', (req, res)=> {
+    res.sendFile(__dirname + "/views/index.html");
+    // res.send('Hello Express');
+})
 
+app.get('/json', (req,res)=> {
+    res.json({"message":"Hello json"});
+})
+
+// console.log("Hello World");
 
 
 
