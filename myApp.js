55a5c944f8b13510c94f22c4350ffb91cbfd2fd8
Bhaba <8bhaba8@gmail.com>
2024-01-20 01:54:25 +0530

    Modifed app.js based on exercise

--- myApp.js
+++ myApp.js
@@ -1,16 +1,41 @@
 let express = require('express');
 let app = express();
 
+require('dotenv').config()
+
+app.use((req, res, next) => {
+    console.log(req.method +" "+ req.path + " - " + req.ip);
+    next();
+});
+
+app.get('/:word/echo', (req, res) => {
+    res.json({"echo" : req.params.word })
+})
+
+app.get('/now',(req, res, next) => {
+    req.time = new Date().toString(); 
+    next();
+}, (req, res) => {
+    res.json({"time": req.time});
+}
+)
+
 app.get('/', (req, res)=> {
     res.sendFile(__dirname + "/views/index.html");
     // res.send('Hello Express');
 })
 
-app.get('/json', (req,res)=> {
-    res.json({"message":"Hello json"});
-})
+// app.get('/json', (req,res)=> {
+//     res.json({"message":"Hello json"});
+// })
 
-// console.log("Hello World");
+app.get('/json', (req, res) => {
+    if(process.env.MESSAGE_STYLE == 'uppercase') {
+        res.json({"message" : "HELLO JSON"})
+    } else {
+        res.json({"message" : "Hello json"})
+    }
+})
 
 
 
